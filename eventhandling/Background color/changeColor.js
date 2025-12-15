066e0bfe92b72c490874668831d3de96460c8b64
addie-hue <adhithigatty0@gmail.com>
2025-12-15 13:25:59 +0530

    changes

--- eventhandling/Background color/changeColor.js
+++ eventhandling/Background color/changeColor.js
@@ -1,27 +1,14 @@
-function greenColor(){
-document.body.style.backgroundColor="green";
+function getRandomColor()
+{
+     var letters ='0123456789ABCDEF';
+      var color= '#';
+      for(var i=0;i<6;i++)
+        { 
+            color += letters[Math.floor(Math.random()*16)];
+        } 
+     return color; 
 }
-function blueColor(){
-document.body.style.backgroundColor="blue";
-}
-function yellowColor(){
-document.body.style.backgroundColor="yellow";
-}
-function orangeColor(){
-document.body.style.backgroundColor="orange";
-}
-function pinkColor(){
-document.body.style.backgroundColor="pink";
-}
-function brownColor(){
-document.body.style.backgroundColor="brown";
-}
-function indigoColor(){
-document.body.style.backgroundColor="indigo";
-}
-function voiletColor(){
-document.body.style.backgroundColor="voilet";
-}
-function redColor(){
-document.body.style.backgroundColor="red";
+function changeBackgroundColor() 
+{ 
+    document.body.style.backgroundColor = getRandomColor(); 
 }
